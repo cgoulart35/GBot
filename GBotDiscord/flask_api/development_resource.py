891d5d2ee57b4f20f667c7b42af07a040f0cc321
Christopher Goulart <cgoulart35@gmail.com>
2022-05-31 19:13:34 -0400

    Fixing bug in GitProjectUpdateHandler post request

--- GBotDiscord/flask_api/development_resource.py
+++ GBotDiscord/flask_api/development_resource.py
@@ -23,11 +23,11 @@ class Development():
             if "doRebuildLatest" in value and value["doRebuildLatest"] == True:
                 response = await Development.sendRequestToGitUpdaterHost()
                 if response == None:
-                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Can't communicate with the Git Updater API."}
+                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Can't communicate with the Git Project Update Handler API."}
                 if "status" not in response:
-                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Missing status in response from Git Updater API."}
+                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Missing status in response from Git Project Update Handler API."}
                 if "message" not in response:
-                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Missing message in response from Git Updater API."}
+                    return {"action": "doRebuildLatest", "status": "failure", "message": "Error: Missing message in response from Git Project Update Handler API."}
                 return {"action": "doRebuildLatest", "status": response["status"], "message": response["message"]}
         except:
             abort(400, "Error: Unhandled exception.")
@@ -37,7 +37,7 @@ class Development():
             async with httpx.AsyncClient() as httpxClient:
                 url = Development.GIT_UPDATER_HOST
                 response = None
-                response = await httpxClient.post(url, data = {'application': 'GBot'})
+                response = await httpxClient.post(url, data = json.dumps({"application": "GBot"}))
                 if response == None or response.status_code != 200:
                     return None
                 return response.json()
